241b4427b67809b35726b317972f6970f4e60d97
efchatz <43434138+efchatz@users.noreply.github.com>
2023-11-04 03:04:59 +0200

    Create getCredspasswordbossapp1.h

---
+++ headers/passwordboss/app/getCredspasswordbossapp1.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "../../core/saveFile.h"
+
+int getCredspasswordbossapp1() {
+    std::ifstream file("app.dmp", std::ios::binary);
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening the file." << std::endl;
+        return 1;
+    }
+
+    std::vector<unsigned char> searchPattern = { 0x68, 0x74, 0x74, 0x70, 0x73, 0x25, 0x33, 0x41, 0x25, 0x32, 0x46 };
+    std::vector<unsigned char> foundData;
+
+    while (!file.eof()) {
+        unsigned char c;
+        file.read(reinterpret_cast<char*>(&c), sizeof(c));
+
+        if (c == searchPattern[foundData.size()]) {
+            foundData.push_back(c);
+            if (foundData.size() == searchPattern.size()) {
+                // We found the search pattern, now collect data until reaching the 100 data limit
+                std::vector<unsigned char> extractedData;
+                int dataCount = 0;
+
+                while (dataCount < 100 && !file.eof()) {
+                    file.read(reinterpret_cast<char*>(&c), sizeof(c));
+                    extractedData.push_back(c);
+                    dataCount++;
+                }
+
+                // Convert the binary data to a UTF-8 string
+                std::string utf8ExtractedData(extractedData.begin(), extractedData.end());
+
+                // Print the extracted UTF-8 string
+                std::cout << "Pattern data: " + utf8ExtractedData << std::endl;
+
+                //Save into file
+                saveFile(utf8ExtractedData);
+
+                foundData.clear();
+            }
+        }
+        else {
+            foundData.clear();
+        }
+    }
+
+    file.close();
+    return 0;
+}
