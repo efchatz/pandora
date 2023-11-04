170a2cda61ac446b6b4408da34dbc3c9c1b41e79
efchatz <43434138+efchatz@users.noreply.github.com>
2023-11-04 03:04:08 +0200

    Create getCredsnorton.h

---
+++ headers/norton/getCredsnorton.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "../core/saveFile.h"
+
+int getCredsnorton() {
+    std::ifstream file("app.dmp", std::ios::binary);
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening the file." << std::endl;
+        return 1;
+    }
+
+    std::string searchKeyword = "\"payload\":[{\"key\":";
+    std::string foundData;
+
+    while (!file.eof()) {
+        char c;
+        file.get(c);
+
+        if (c == searchKeyword[foundData.size()]) {
+            foundData += c;
+            if (foundData == searchKeyword) {
+                // We found the search keyword, now collect data until 10 binary spaces (00) are found
+                std::vector<unsigned char> extractedData;
+                int consecutiveSpaces = 0;
+
+                while (!file.eof()) {
+                    file.read(reinterpret_cast<char*>(&c), sizeof(c));
+                    if (c == 0x00) {
+                        consecutiveSpaces++;
+                        if (consecutiveSpaces == 10) {
+                            break; // 10 consecutive binary spaces found
+                            // We can check for more spaces if we want
+                        }
+                    }
+                    else {
+                        consecutiveSpaces = 0;
+                    }
+                    extractedData.push_back(c);
+                }
+
+                // Convert the binary data to a UTF-8 string
+                std::string utf8ExtractedData(extractedData.begin(), extractedData.end());
+
+                // Print the extracted UTF-8 string
+                std::cout << "Pattern Data: " + utf8ExtractedData << std::endl;
+
+                //Save into file
+                saveFile(utf8ExtractedData);
+
+                foundData.clear();
+            }
+        }
+        else {
+            foundData.clear();
+        }
+    }
+
+    file.close();
+    return 0;
+}
