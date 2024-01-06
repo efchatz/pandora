ee5cb87a0cb51d3a902564f32ba6e486e53f38ab
efchatz <43434138+efchatz@users.noreply.github.com>
2024-01-06 17:42:04 +0200

    Update getCredsfirefox.h

--- headers/firefox/getCredsfirefox.h
+++ headers/firefox/getCredsfirefox.h
@@ -6,8 +6,8 @@
 #include <string>
 #include "../core/saveFile.h"
 
-int getCredsfirefox() {
-    std::ifstream file("app.dmp", std::ios::binary);
+int getCredsfirefox(std::string filename) {
+    std::ifstream file(filename, std::ios::binary);
 
     if (!file.is_open()) {
         std::cerr << "Error opening the file." << std::endl;
