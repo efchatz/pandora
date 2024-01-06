55cb8d1ebb8eec7e4e320659881ba3a34c092fbc
efchatz <43434138+efchatz@users.noreply.github.com>
2024-01-06 17:44:35 +0200

    Update getCredsnorton.h

--- headers/norton/getCredsnorton.h
+++ headers/norton/getCredsnorton.h
@@ -5,8 +5,8 @@
 #include <vector>
 #include "../core/saveFile.h"
 
-int getCredsnorton() {
-    std::ifstream file("app.dmp", std::ios::binary);
+int getCredsnorton(std::string filename) {
+    std::ifstream file(filename, std::ios::binary);
 
     if (!file.is_open()) {
         std::cerr << "Error opening the file." << std::endl;
