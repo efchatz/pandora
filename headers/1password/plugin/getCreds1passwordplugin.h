0d08efb790fd2c5487816ffd93d02da1e2660ce0
efchatz <43434138+efchatz@users.noreply.github.com>
2024-01-06 17:38:53 +0200

    Update getCreds1passwordplugin.h

--- headers/1password/plugin/getCreds1passwordplugin.h
+++ headers/1password/plugin/getCreds1passwordplugin.h
@@ -5,8 +5,8 @@
 #include <string>
 #include "../../core/saveFile.h"
 
-void getCreds1passwordplugin() {
-    std::ifstream file("app.dmp", std::ios::binary);
+void getCreds1passwordplugin(std::string filename) {
+    std::ifstream file(filename, std::ios::binary);
 
     if (!file.is_open()) {
         std::cerr << "Error opening the file." << std::endl;
