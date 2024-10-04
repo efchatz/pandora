9a8858c9b749eded0ac2152cceb868f8a4e337e4
efchatz <43434138+efchatz@users.noreply.github.com>
2024-10-04 15:48:32 +0300

    Update getCredsavira.h

--- headers/avira/getCredsavira.h
+++ headers/avira/getCredsavira.h
@@ -13,7 +13,7 @@ int getCredsavira(std::string filename) {
         return 1;
     }
 
-    std::vector<unsigned char> searchPattern = { 0x21, 0x6a, 0x19, 0x00, 0x19, 0x02, 0x00, 0x00, 0x19, 0x02, 0x00,0x00, 0x00, 0x00, 0x00, 0x00, 0x8d, 0x05, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00 };
+    std::vector<unsigned char> searchPattern = { 0x00, 0x59, 0xe4, 0x38, 0x00, 0x75, 0x07, 0x00, 0x00, 0x75, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x59, 0xe4, 0x38, 0x00, 0x75, 0x07, 0x00, 0x00, 0x75, 0x07, 0x00 };
     std::vector<unsigned char> foundData;
 
     while (!file.eof()) {
@@ -23,11 +23,11 @@ int getCredsavira(std::string filename) {
         if (c == searchPattern[foundData.size()]) {
             foundData.push_back(c);
             if (foundData.size() == searchPattern.size()) {
-                // We found the search pattern, now collect data until reaching the 600 data limit
+                // We found the search pattern, now collect data until reaching the 300 data limit
                 std::vector<unsigned char> extractedData;
                 int dataCount = 0;
 
-                while (dataCount < 600 && !file.eof()) {
+                while (dataCount < 300 && !file.eof()) {
                     file.read(reinterpret_cast<char*>(&c), sizeof(c));
                     extractedData.push_back(c);
                     dataCount++;
